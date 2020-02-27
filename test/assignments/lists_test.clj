93d24e3fb35587c96dab40f8f534a42e1748fb9e
Anoop Elias <Anoop.Elias@thoughtworks.com>
2020-02-27 14:45:12 +0530

    Improved test cases for map'
    Signed-off-by: Anoop Elias <Anoop.Elias@thoughtworks.com>

--- test/assignments/lists_test.clj
+++ test/assignments/lists_test.clj
@@ -5,10 +5,10 @@
 (deftest lists
   (testing "map"
     (testing "identity with single coll"
-      (is (= [1 2 3] (map' identity [1 2 3])))))
-  (testing "map"
+      (is (= [1 2 3] (map' identity [1 2 3]))))
     (testing "inc with single coll"
-      (is (= [3 4 5] (map' (comp inc inc) [1 2 3])))))
-  (testing "map"
+      (is (= [3 4 5] (map' (comp inc inc) [1 2 3]))))
     (testing "vector with multiple coll"
-      (is (= [[1 4] [2 5] [3 6]] (map' vector [1 2 3] [4 5 6]))))))
+      (is (= [[1 4] [2 5] [3 6]] (map' vector [1 2 3] [4 5 6]))))
+    (testing "vector with multiple multisized coll"
+      (is (= [[1 4] [2 5]] (map' vector [1 2 3] [4 5]))))))
